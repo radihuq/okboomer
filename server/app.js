467daf95640c8d051dce881bf73ca0e4ab078b9a
Radiun Huq <huqradi@gmail.com>
2019-12-12 16:50:59 -0500

    set inital config

--- server/app.js
+++ server/app.js
@@ -2,9 +2,10 @@ const express = require('express');
 const app = express();
 const mongoose = require('mongoose');
 const cors = require('cors');
+require('dotenv/config');
 
-const SERVER_PORT = 3000;
-const DB_CONNECTION = ``;
+const SERVER_PORT = process.env.SERVER_PORT;
+const DB_CONNECTION = process.env.DB_CONNECTION;
 
 //Connect to DB
 mongoose.connect(DB_CONNECTION, {useNewUrlParser: true, useUnifiedTopology: true}, () => {
